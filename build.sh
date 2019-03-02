744719cf91ef5b07c0a43e3ec32a88eb8505c8bf
ntyukaev <nikolaityukaev@gmail.com>
2019-03-02 22:51:12 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-$POSTGRES_PASSWORD=$1
+POSTGRES_PASSWORD=$1
+echo $POSTGRES_PASSWORD
 docker-compose build --build-arg SSH_PRIVATE_KEY="`echo "$(cat ~/.ssh/id_rsa)"`" --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+docker-compose up
