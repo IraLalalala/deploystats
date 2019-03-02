ec5d17a039b1d9c83448e72a4dd1364d77de2fa2
ntyukaev <nikolaityukaev@gmail.com>
2019-03-02 23:23:18 +0300

    add pgadmin

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 POSTGRES_PASSWORD=$1
 echo $POSTGRES_PASSWORD
 docker-compose build --build-arg SSH_PRIVATE_KEY="`echo "$(cat ~/.ssh/id_rsa)"`" --build-arg POSTGRES_PASSWORD=$POSTGRES_PASSWORD
-docker-compose up
+docker-compose up -d
